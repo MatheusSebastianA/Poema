6555846f416a738a2b39fba227841e13fa73045c
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-10 12:59:13 -0300

    Add files via upload

---
+++ poema.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    int posi;
+    char letra;
+    FILE *arq, *arq_dst;
+
+    arq = fopen("poema.bin", "r");
+
+    if(!arq){
+        perror("Arquivo não foi aberto");
+        return 1;
+    }
+
+    arq_dst = fopen("saida.txt", "w");
+
+    if(!arq_dst){
+        perror("Arquivo não foi aberto");
+        return 1;
+    }
+
+    fread(&posi, sizeof(int), 1, arq);
+    while (! feof(arq)){
+        fread(&letra, sizeof(char), 1, arq);
+        fseek (arq_dst, posi, SEEK_SET);
+        fprintf(arq_dst, "%c", letra);
+        fread(&posi, sizeof(int), 1, arq);
+    }
+
+    fclose(arq);
+    return 0;
+}   
